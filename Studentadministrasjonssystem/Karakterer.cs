7778ac141ad047fcddfbd3023f1327858d17336d
mattiasfe <matferge@gmail.com>
2025-05-27 12:48:27 +0200

    gg

--- Studentadministrasjonssystem/Karakterer.cs
+++ Studentadministrasjonssystem/Karakterer.cs
@@ -1,16 +1,6 @@
-﻿// namespace Studentadministrasjonssystem;
-//
-// public class Karakterer
-// {
-//     private int karakterer;
-//
-//     public Karakterer(int karakterer)
-//     {
-//         this.karakterer = karakterer;
-//     }
-//     
-//     public void GetKarakterer(int karakterer)
-//     {
-//         Console.WriteLine($"Karakterer: {karakterer}");
-//     }
-// }
+﻿namespace Studentadministrasjonssystem;
+
+public class Karakterer
+{
+    private int _karakter;
+}
